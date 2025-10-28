1d4b2964cde2174fa0d56d63afb38ea24f3fab0f
Pantham Charan Naga Babu <Killerworth23@gmail.com>
2025-10-28 12:00:00 +0530

    Initial Setup and dependencies added

---
+++ app/src/main/java/uk/ac/tees/mad/ridehive/MainActivity.kt
@@ -0,0 +1,55 @@
+package uk.ac.tees.mad.ridehive
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uk.ac.tees.mad.ridehive.ui.theme.RideHiveTheme
+import java.io.File
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RideHiveTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val osName = System.getProperty("os.name")
+    val cores = Runtime.getRuntime().availableProcessors()
+    val deviceModel = Build.MODEL
+    val deviceCpu = Build.SUPPORTED_ABIS.joinToString(", ") // CPU architectures
+
+    Text(
+        text = "Hello $name! I am running on $osName with $cores cores.\n" +
+                "Device: $deviceModel\nCPU Architectures: $deviceCpu",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RideHiveTheme {
+        Greeting("Android")
+    }
+}
